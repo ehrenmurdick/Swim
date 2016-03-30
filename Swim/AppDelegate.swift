3707913314fb813ba6395affe768765b912304c8
Ehren Murdick <pair+emurdick@pivotallabs.com>
2016-03-30 14:37:18 -0700

    Initial Commit

---
+++ Swim/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swim
+//
+//  Created by Ehren Murdick on 3/30/16.
+//  Copyright © 2016 Ehren Murdick. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
